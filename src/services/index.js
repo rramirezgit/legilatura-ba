864a4df51928ed64f6a49f8d194f2de795b7a941
Ricardo <rramirez@lukapay.com>
2022-12-28 11:11:42 -0300

    wip

--- src/services/index.js
+++ src/services/index.js
@@ -5,6 +5,10 @@ const api = axios.create({
   mode: "no-cors",
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept",
   },
 });
 
