df5fa54de0e43a5eaaf3b6e1a1636bd5569d3a72
Ricardo <rramirez@lukapay.com>
2023-01-04 09:50:48 -0300

    wip

--- src/pages/listaTramites/ListaTamitesLogica.js
+++ src/pages/listaTramites/ListaTamitesLogica.js
@@ -1,5 +1,7 @@
 import { masterCertificationList } from "../../services";
 
+const fromatDate = (date) => new Date(date).toLocaleDateString();
+
 export const getListaTramites = (cuil, setRows) => {
   const data = masterCertificationList(cuil)
     .then((response) => {
@@ -7,8 +9,16 @@ export const getListaTramites = (cuil, setRows) => {
       if (response.statusText === "OK") {
         if (response.data.length === 0) {
           setRows([]);
+        } else {
+          let data = {
+            ...response.data,
+            fechaCreacion: fromatDate(response.data.fechaCreacion),
+            fechaCertificacion: fromatDate(response.data.fechaCertificacion),
+            fechaDecision: fromatDate(response.data.fechaDecision),
+          };
+
+          setRows(data);
         }
-        setRows(response.data);
       } else {
         setRows([]);
       }
