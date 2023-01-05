63041c9a0d63f91675fcd86e372bb83fa3213745
Ricardo <rramirez@lukapay.com>
2023-01-05 12:21:08 -0300

    wip

--- src/components/common/DateRange.jsx
+++ src/components/common/DateRange.jsx
@@ -1,6 +1,6 @@
 import { Autocomplete, Input, TextField } from "@mui/material";
 import { useGridApiContext } from "@mui/x-data-grid";
-import { forwardRef, useState } from "react";
+import { useState } from "react";
 import { IMaskInput } from "react-imask";
 // One time slot
 const timeSlots = Array.from(new Array(24)).map((_, i) => {
@@ -13,9 +13,10 @@ const TextMaskCustom = (props) => {
   return (
     <IMaskInput
       {...other}
-      mask="(#00) 000-0000"
+      mask="#0-#0"
       definitions={{
-        "#": /[1-9]/,
+        "#": /[0-2]/,
+        0: /[0-9]/,
       }}
       onAccept={(value) => onChange({ target: { name: props.name, value } })}
       overwrite
@@ -24,62 +25,12 @@ const TextMaskCustom = (props) => {
 };
 
 export const DateRange = ({ id, value, field }) => {
-  const [values, setValues] = useState({
-    textmask: "(100) 000-0000",
-    numberformat: "1320",
-  });
-  const handleChange = (event) => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value,
-    });
-  };
+  console.log(value);
+  const handleChange = (event) => {};
   const apiRef = useGridApiContext();
   return [
-    // <Autocomplete
-    //   options={timeSlots}
-    //   sx={{ width: 80 }}
-    //   defaultValue={value.split("-")[0]}
-    //   onChange={(e, value) => {
-    //     console.log(value);
-    //     apiRef.current.setEditCellValue({
-    //       id: id,
-    //       field,
-    //       value,
-    //     });
-    //   }}
-    //   disableClearable
-    //   renderInput={(params) => (
-    //     <TextField {...params} value={value.split("-")[0]} />
-    //   )}
-    // />,
-    // <Autocomplete
-    //   options={timeSlots}
-    //   defaultValue={() => {
-    //     if (typeof value === "string") {
-    //       return value.split("-")[1];
-    //     } else {
-    //       if (value.length < 10) {
-    //         return `0${value}`;
-    //       }
-    //     }
-    //   }}
-    //   onChange={(e, value) => {
-    //     console.log(value);
-    //     apiRef.current.setEditCellValue({
-    //       id: id,
-    //       field,
-    //       value,
-    //     });
-    //   }}
-    //   sx={{ width: 80 }}
-    //   disableClearable
-    //   renderInput={(params) => (
-    //     <TextField {...params} value={value.split("-")[1]} />
-    //   )}
-    // />,
     <Input
-      value={values.textmask}
+      value={value}
       onChange={handleChange}
       name="textmask"
       id="formatted-text-mask-input"
