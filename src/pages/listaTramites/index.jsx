e2da4548ea005ea399c51a5ac3b941a77fd160ed
Ricardo <rramirez@lukapay.com>
2023-01-08 20:38:11 -0300

    wip

--- src/pages/listaTramites/index.jsx
+++ src/pages/listaTramites/index.jsx
@@ -33,9 +33,7 @@ export default function ListaTramites() {
 
   useEffect(() => {
     if (componentRef?.current) {
-      setTimeout(() => {
-        handlePrint();
-      }, 1000);
+      handlePrint();
     }
   }, [PDFDocument]);
 
