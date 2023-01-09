503a9055a9810509029a00c37089ba5cd9031c82
Ricardo <rramirez@lukapay.com>
2023-01-09 16:55:38 -0300

    wip

--- src/pages/certificaciones/index.jsx
+++ src/pages/certificaciones/index.jsx
@@ -241,7 +241,7 @@ export default function Certificaciones() {
               onClick={() => {
                 postMasterCertification({
                   data: {
-                    periodo: new Date(periodo).toISOString().slice(0, 7),
+                    periodo: periodo,
                     dependencia: user.IdDependency,
                     cuitCertificante: user.Cuil,
                     estado: "B",
