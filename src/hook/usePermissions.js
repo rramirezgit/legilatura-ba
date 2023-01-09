e489de0215cb9c8d5d6960357f2cba351e2c4791
Ricardo <rramirez@lukapay.com>
2023-01-08 22:22:10 -0300

    wip

--- src/hook/usePermissions.js
+++ src/hook/usePermissions.js
@@ -19,8 +19,7 @@ const usePermissions = (state, periodo) => {
       canSign:
         ProfileDesc === "Director" &&
         state === "B" &&
-        new Date(periodo).toISOString().slice(0, 7) ===
-          new Date().toISOString().slice(0, 7),
+        periodo === new Date().toISOString().slice(0, 7),
     });
   }, [ProfileDesc, state]);
 
