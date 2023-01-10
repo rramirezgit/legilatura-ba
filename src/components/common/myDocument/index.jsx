52e6c175031ea63225dd4f931a6ce9a37784df25
Ricardo <rramirez@lukapay.com>
2023-01-09 21:21:44 -0300

    wip

--- src/components/common/myDocument/index.jsx
+++ src/components/common/myDocument/index.jsx
@@ -83,7 +83,7 @@ const MyDocument = ({
                               if (col.field === "estado") {
                                 return (
                                   <TableCell key={index}>
-                                    {row[col.field] === "0" ? "no" : "si"}
+                                    {row[col.field] ? "si" : "no"}
                                   </TableCell>
                                 );
                               }
